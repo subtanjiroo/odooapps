aeb3e856f3b0e93fb4961d3c1ece153053c5f73a
minh <deadshoflife366@gmail.com>
2025-06-12 15:05:14 +0700

    upgrade

--- leandix_ai/__manifest__.py
+++ leandix_ai/__manifest__.py
@@ -11,11 +11,6 @@
 # See LICENSE file for full license terms.
 # Note: When using Leandix AI, selected business data (such as chat prompts, relevant model fields, or context-specific metadata) will be securely transmitted to a remote AI processing server (via API) for analysis and response generation. This allows AI services to understand your context and deliver accurate results.
 
-
-
-
-
-# NHỚ XÓA PGADMINNNNNNNNNNNNNNNNNNNNNNNNNNNNNN!!!!!!!!!!!!!!!!!!!!!!!!!!!! ( nhớ xóa cả dòng này )
 {
     'name': 'Leandix AI',
     'version': '18.0.1.0.0',
@@ -46,7 +41,7 @@
 
         ]
     },
-    "images": ["static/description/icon.png"],
+    "images": ["static/description/banner.jpg"],
     'installable': True,
     'application': True,
     'auto_install': False,
